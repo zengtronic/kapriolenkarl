3a6e8fa4f397f65760ab1d4442761227c4bf85a4
Tobias Kozel <t.kozel@protonmail.com>
2020-09-10 19:11:50 +0200

    init

---
+++ scenes/lobby/Lobby.gd
@@ -0,0 +1,96 @@
+extends Control
+
+var peer: WebSocketClient = null
+
+var players = []
+
+export(NodePath) var name_field
+export(NodePath) var address_field
+export(NodePath) var player_list
+
+func _ready():
+	get_tree().connect("network_peer_connected", self, "_player_connected")
+	get_tree().connect("network_peer_disconnected", self, "_player_disconnected")
+
+
+sync func update_players(p):
+	print(p)
+	players = p
+	get_node(player_list).clear()
+	for player in p:
+		get_node(player_list).add_item(player.name)
+
+
+func _player_connected(id):
+	pass
+	
+
+master func register_player(name):
+	var sender = get_tree().get_rpc_sender_id()
+	var newList = players
+	for player in newList:
+		if player.name == name:
+			return
+	newList.append({
+		"id": sender,
+		"name": name,
+		"ready": false
+	})
+	for player in newList:
+		rpc_id(player.id, "update_players", newList)
+	update_players(newList)
+
+
+func _player_disconnected(id):
+	if not get_tree().is_network_server():
+		return
+	var p = null
+	for player in players:
+		if player.id == id:
+			p = player
+	if (!p):
+		print("Couldn't find player to remove")
+		return
+	var newList = players
+	newList.erase(p)
+	for player in newList:
+		rpc_id(player.id, "update_players", newList)
+	update_players(newList)
+
+
+func _close_network():	
+	if get_tree().is_connected("connection_failed", self, "_close_network"):
+		get_tree().disconnect("connection_failed", self, "_close_network")
+	if get_tree().is_connected("connected_to_server", self, "_connected"):
+		get_tree().disconnect("connected_to_server", self, "_connected")
+	if (peer):
+		peer.disconnect_from_host()
+	get_tree().set_network_peer(null)
+	peer = null
+	get_node(player_list).clear()
+	print("Disconnected")
+
+
+func _connected():
+	if get_tree().is_network_server():
+		return
+	# register to the server with the name
+	rpc("register_player", get_node(name_field).text)
+
+
+func _on_connect_pressed():
+	if get_tree().is_network_server():
+		return
+	if peer:
+		_close_network()
+	var host = get_node(address_field).text
+	peer = WebSocketClient.new()
+	get_tree().connect("connection_failed", self, "_close_network")
+	get_tree().connect("connected_to_server", self, "_connected")
+	peer.connect_to_url("ws://" + host, PoolStringArray(), true)
+	get_tree().set_network_peer(peer)
+
+
+func _on_ready_pressed():
+	if (!peer):
+		return
